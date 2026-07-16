ad6e380fc596d8234121beda1d0b7246a05b28be
amitScript <amitk400733@gmail.com>
2026-07-16 16:41:38 +0530

    Maximum Subarray

---
+++ LeetCode/0053-Maximum-Subarray/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        
+
+        int max_sum = INT_MIN;
+        int n = nums.size();
+        int curr_sum;
+
+
+    for(int i = 0; i < n; i++) {
+          curr_sum += nums[i];
+          max_sum = max(max_sum, curr_sum);
+
+          if(curr_sum<0){
+            curr_sum =0;
+          }
+    }
+    
+    return max_sum;
+    }
+};
